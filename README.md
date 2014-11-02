758587dfef3684399b8ed65748ddbaf6c368a0dd
storayy <dcanenasus@gmail.com>
2014-11-02 01:26:17 -0400

    files

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-nodebb-plugin-emoji-twitch
-==========================
+# Twitch Emoji for NodeBB
 
-Twitch Emoji for NodeBB
+This NodeBB plugin allows users to use Twitch Emoticons.
+
+# Example:
+* `Kappa` = ![Kappa](http://i.imgur.com/ZFlCigT.png)
+
+## Emoticons
+
+    Kappa Keepo FrankerZ BibleThump ShibeZ DansGame FailFish KAPOW MrDestructoid BloodTrail PJSalt SMOrc
---
+++ index.js
@@ -0,0 +1,37 @@
+var	nconf = module.parent.require('nconf'),
+	Emoji = {
+		mapping: {
+			"Kappa": "Kappa",
+			"BibleThump": "BibleThump",
+			"DansGame": "DansGame",
+			"FrankerZ": "FrankerZ",
+			"MrDestructoid": "MrDestructoid",
+			"ShibeZ": "ShibeZ",
+			"BloodTrail": "BloodTrail",
+			"FailFish": "FailFish",
+			"KAPOW": "KAPOW",
+			"Keepo": "Keepo",
+			"PJSalt": "PJSalt",
+			"SMOrc": "SMOrc",
+		}
+	};
+
+	Emoji.replaceOutsideOfCode = function (content, pattern, cb) {
+		return content.replace(/(^|<\/code>)([^<]*|<(?!code>))*(<code>|$)/g, function (match) {
+			return match.replace(pattern, cb);
+		});
+	};
+
+	Emoji.addEmoji = function (postContent, callback) {
+		callback(null, Emoji.replaceOutsideOfCode(postContent,
+			/&gt;:\)|\([\w~]+\)|\\[:]?[od]\/|[:;\|bBiIxX8\(\)\]][=\-"^:]?[)>$&|\w\(\)*@#?]?[)>$&|\w\(\)*@#?]/g,
+			function (match) {
+				match = match.replace('&gt;', '>');
+				return Emoji.mapping[match] ? '<img class="nodebb-plugin-emoji-twitch" src="' +
+					nconf.get('relative_path') + '/plugins/nodebb-plugin-emoji-twitch/icons/' +
+					Emoji.mapping[match] + '.png" title="' + match + '"/>' : match;
+			}
+		));
+	};
+
+module.exports = Emoji;
